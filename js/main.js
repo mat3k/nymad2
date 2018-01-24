a780873efd97d94b9ecd49f01679f29d34c8a7a2
Mateusz Baltyn <mat3kbn@gmail.com>
2018-01-24 10:25:51 +0100

    Switch lambda functions to arrow functions

--- js/main.js
+++ js/main.js
@@ -5,14 +5,6 @@ let images = ['build/assets/monsters-32x32.png', 'build/assets/wiptiles.png']
 
 game.preloadImages(images)
 
-window.onload = function() {
-  game.start();
-}
-
-window.addEventListener("keydown", (e) => {
-  game.controller.addKey(e.keyCode);
-});
-
-window.addEventListener("keyup", (e) => {
-  game.controller.removeKey(e.keyCode);
-});
+window.onload = () => game.start();
+window.addEventListener('keydown', (e) => game.controller.addKey(e.keyCode));
+window.addEventListener('keyup', (e) => game.controller.removeKey(e.keyCode));
