cbc434177cb1734a8ab0548115b8cc7b8eb247fb
Mateusz Baltyn <mat3kbn@gmail.com>
2018-02-10 12:44:39 +0100

    Add collision detection on arena

--- js/monster.js
+++ js/monster.js
@@ -1,13 +1,15 @@
 import SpriteImage from './sprite_image';
+import Position from './position';
 
 export default class Monster {
   constructor(data) {
     this.name = data.name;
-    this.x = 30;
-    this.y = 30;
+    this.arenaPosition = new Position(30, 30);
     this.image = new SpriteImage(this.getSprite(), data.sx, data.sy);
     this.maxHP = data.maxHP;
     this.currentHP = this.maxHP;
+    this.width = 32;
+    this.height = 32;
   }
 
   getSprite() {
