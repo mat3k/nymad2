7d95d301c2149b7e5deeaa406cbc304c3fad81a7
Mateusz Baltyn <mat3kbn@gmail.com>
2018-03-02 15:55:38 +0100

    Update game loop

--- js/ticker.js
+++ js/ticker.js
@@ -1,22 +1,23 @@
 class Ticker {
   constructor(updateFn, drawFn) {
-    this.lastFrameTimeMs = 0;
-    this.maxFPS = 30;
+    this.last = window.performance.now();
+    this.step = 1/60;
+    this.dt = 0;
 
     this.updateFn = updateFn;
     this.drawFn = drawFn;
   }
 
-  loop(timestamp) {
-    if (timestamp < this.lastFrameTimeMs + (1000 / this.maxFPS)) {
-      requestAnimationFrame(() => { this.loop(); });
-      return;
+  loop() {
+    let now = window.performance.now();
+    this.dt = this.dt + Math.min(1, (now - this.last) / 1000);
+    while(this.dt > this.step) {
+      this.dt = this.dt - this.step;
+      this.updateFn();
     }
-    this.lastFrameTimeMs = timestamp;
-
-    this.updateFn();
     this.drawFn();
-    requestAnimationFrame(() => { this.loop(); });
+    this.last = now;
+    requestAnimationFrame(() => this.loop());
   }
 }
 
