7acfdf572835c747e716fc55a3537c086e194cb6
Mateusz Baltyn <mat3kbn@gmail.com>
2018-03-13 10:51:20 +0100

    Rename directory with source files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './js/main.js',
+    entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'main.bundle.js'
